2d8b6fd18de5348caf681388c8b4821e60424958
Pedro Lucas Garcia <190115548@aluno.unb.br>
2024-08-15 16:42:34 -0300

    Configura mvp com docker

---
+++ frontend/Dockerfile
@@ -0,0 +1,7 @@
+FROM node:20
+WORKDIR /app
+COPY . .
+RUN npm install
+RUN npm install @angular/cli -g
+EXPOSE 4200
+CMD [ "ng", "serve", "--host", "0.0.0.0"]
---
+++ mvp/src/main/java/com/mymarket/mvp/MvpApplication.java
@@ -0,0 +1,13 @@
+package com.mymarket.mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpApplication.class, args);
+	}
+
+}
